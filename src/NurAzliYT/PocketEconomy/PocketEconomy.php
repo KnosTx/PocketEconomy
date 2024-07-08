a97333fe703906edda433032b2650917b0ec3fe3
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-08 20:26:07 +0700

    Update PocketEconomy.php

--- src/NurAzliYT/PocketEconomy/PocketEconomy.php
+++ src/NurAzliYT/PocketEconomy/PocketEconomy.php
@@ -1,23 +1,5 @@
 <?php
 
-/*
- * EconomyS, the massive economy plugin with many features for PocketMine-MP
- * Copyright (C) 2013-2017  NurAzliYT <jyc00410@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace NurAzliYT\PocketEconomy;
 
 use pocketmine\permission\DefaultPermissions;
@@ -366,7 +348,6 @@ class PocketEconomy extends PluginBase implements Listener
         if (!$this->provider->accountExists($player)) {
             $this->getLogger()->debug("Account of '" . $player->getName() . "' is not found. Creating account...");
             $this->createAccount($player, false, true);
-            $this->langList();
         }
     }
 
