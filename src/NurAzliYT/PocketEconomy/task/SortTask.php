b4cffd7971de549153bfa9010c24a0b189eef269
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-08 20:28:04 +0700

    Update SortTask.php

--- src/NurAzliYT/PocketEconomy/task/SortTask.php
+++ src/NurAzliYT/PocketEconomy/task/SortTask.php
@@ -33,14 +33,6 @@ class SortTask extends AsyncTask
 
     private $topList;
 
-    /**
-     * @param string $player
-     * @param array $moneyData
-     * @param bool $addOp
-     * @param int $page
-     * @param array $ops
-     * @param array $banList
-     */
     public function __construct(private string $sender, private array $moneyData, private bool $addOp, private int $page, private array $ops, private array $banList) {}
 
     public function onRun(): void
